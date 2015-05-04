6c7d2aca792eaf8946198ecdcabf53e20e1fa1f6
Anna Szpoton <anna.szpoton@codilime.com>
2015-05-04 11:51:36 +0200

    Simple node name transformation.
    Replace underscore with hyphen.

--- plugin/gcp/utils.py
+++ plugin/gcp/utils.py
@@ -14,12 +14,38 @@
 #    * limitations under the License.
 
 
-def get_item_from_gcp_response(key_field, key_name, items):
+def get_item_from_gcp_response(name, items):
+    """
+    Get item from GCP REST response JSON list by name
+    :param name: item name
+    :param items: list of items(dictionaries)
+    :return: item if found in collection, None otherwise
+    """
     for item in items.get('items', []):
-        if item.get(key_field) == key_name:
+        if item.get('name') == name:
             return item
     return None
 
 
 def get_firewall_rule_name(network, firewall):
+    """
+    Prefix firewall rule name with network name
+    :param network: name of the network the firewall rule is connected to
+    :param firewall: the firewall rule name
+    :return: network prefixed firewall rule name
+    """
     return '{0}-{1}'.format(network, firewall['name'])
+
+
+def get_instance_name(node_name):
+    """
+    Create GCP accepted instance name. From GCP specification:
+    "Specifically, the name must be 1-63 characters long and match the regular
+    expression [a-z]([-a-z0-9]*[a-z0-9])? which means the first character must
+    be a lowercase letter, and all following characters must be a dash,
+    lowercase letter, or digit, except the last character,
+    which cannot be a dash."
+    :param node_name: name given in blueprint(could have underscore)
+    :return: GCP accepted instance name
+    """
+    return node_name.replace('_', '-')
