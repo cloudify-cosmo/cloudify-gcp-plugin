e31807bafadf1a54a6ec187ee65f96dac5ece187
Isaac Shabtay <isaac@shabtay.com>
2016-10-22 02:16:21 -0700

    Added offline info

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 
     name='cloudify-gcp-plugin',
 
-    version='1.3.1.post5',
+    version='1.3.1.post6',
     description='Plugin for Google Cloud Platform',
 
     packages=['gcp', 'gcp/compute'],
