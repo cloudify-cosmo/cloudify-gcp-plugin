e9f4a1de8ea61586f04c17f04d04e0dd8b7f27d2
AdarShaked <adar@cloudify.co>
2020-05-06 14:46:49 +0300

    added to circle ci py3.6 flake8 and ignore compat file when validating py3 compatability

--- cloudify_gcp/compute/tests/test_instance.py
+++ cloudify_gcp/compute/tests/test_instance.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import  json
+import json
 from functools import partial
 
 from mock import patch, Mock
