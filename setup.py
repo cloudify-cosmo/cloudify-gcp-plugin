b8ec00b280eb030cfea7449ed5979fa19c77d6f3
EarthmanT <EarthmanT@users.noreply.github.com>
2020-12-08 12:32:13 +0200

    upgrade dependencies (#126)

--- setup.py
+++ setup.py
@@ -53,9 +53,10 @@ setup(
         "oauth2client==4.1.3",
         "google-api-python-client==1.7.11",
         "cloudify-common>=4.5.5",
-        "pyyaml==3.12",
+        "pyyaml>=4.2b1",
+        "networkx==1.9.1",
         "pycrypto==2.6.1",
         "jsonschema==3.0.0",
-        "httplib2==0.17.3",
+        'httplib2>=0.18.0'
     ],
 )
