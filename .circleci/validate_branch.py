848ebf44022096f1d0e78a1b84322f911476d7a6
Bartosz Kosciug <bartosz@cloudify.co>
2022-09-01 15:40:22 +0200

    test regex

--- .circleci/validate_branch.py
+++ .circleci/validate_branch.py
@@ -1,5 +1,7 @@
 import os
+import re
 
 if __name__ == "__main__":
-    print(os.environ['BRANCH'])
-    print(os.environ['CIRCLE_BRANCH'])
+    branch = re.compile("[0-9.]*-build")
+    if not branch.match(os.environ['CIRCLE_BRANCH']):
+        raise ValueError("This is not build branch! Can't merge to the master.")
