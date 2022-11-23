30b350e9679892696b57bb2384a01fc419ca375e
Jakub Cierlik <jakub.cierlik@amartus.com>
2022-11-23 16:11:04 +0100

    disk resize test WIP

--- cloudify_gcp/compute/tests/test_disk.py
+++ cloudify_gcp/compute/tests/test_disk.py
@@ -45,7 +45,7 @@ class TestGCPDisk(TestGCP):
                     'sizeGb': 'size',
                     'name': 'name'},
                 project='not really a project',
-                zone='a very fake zone'
+                zone='a very fake zone',
                 )
 
         # run one more time for check rerun
@@ -64,7 +64,7 @@ class TestGCPDisk(TestGCP):
         mock_build().disks().delete.assert_called_with(
                 disk='delete_name',
                 project='not really a project',
-                zone='a very fake zone'
+                zone='a very fake zone',
                 )
 
     def test_add_boot_disk(self, *args):
@@ -83,3 +83,21 @@ class TestGCPDisk(TestGCP):
         self.assertIs(
                 self.ctxmock.target.instance.runtime_properties['gcp_disk'],
                 self.ctxmock.source.instance.runtime_properties['gcp_disk'])
+
+    def test_resize(self, mock_build, *args):
+        test_name = 'test_name'
+        test_zone = 'test_zone'
+        test_size = 40
+
+        self.ctxmock.instance.runtime_properties['sizeGb'] = 'initial_value'
+        mock_build().disks().resize().execute.return_value = {'id': 'fake_id', 'name': test_name}
+
+        disk.resize(test_name, test_zone, test_size)
+
+        mock_build.assert_called_once()
+        mock_build().disks().resize.assert_called_with(
+                project='not really a project',
+                name=test_name,
+                zone=test_zone,
+                size=test_size,
+                )
